33f9bb103cf3011d495bcb18e0c8a0ce60c85358
Rafael Camargo <rafacl2207@gmail.com>
2026-07-31 23:51:03 -0300

    Create variaveis.py

---
+++ Módulo 1/variaveis.py
@@ -0,0 +1,19 @@
+# ====================================
+# AULA 01 - VARIÁVEIS
+# ====================================
+
+# Informações do jogador
+
+nome = "Arthur"
+classe = "Guerreiro"
+
+vida = 100
+mana = 50
+ouro = 250
+
+print("=== PERSONAGEM ===")
+print(f"Nome: {nome}")
+print(f"Classe: {classe}")
+print(f"Vida: {vida}")
+print(f"Mana: {mana}")
+print(f"Ouro: {ouro}")
